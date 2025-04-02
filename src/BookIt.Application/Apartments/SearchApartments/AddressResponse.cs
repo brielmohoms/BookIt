0d3a613752c46a1657ea6b6cc5e366af9a9089bd
brielmohoms <brielmohoms@yahoo.com>
2025-04-02 10:48:28 +0200

    created the tables for Apartment and bookings in the database

--- src/BookIt.Application/Apartments/SearchApartments/AddressResponse.cs
+++ src/BookIt.Application/Apartments/SearchApartments/AddressResponse.cs
@@ -11,4 +11,5 @@ public sealed class AddressResponse
     public string City { get; init; }
     
     public string Street { get; init; }
+    
 }
---
+++ src/BookIt.Application/Apartments/SearchApartments/SearchApartmentsQueryHandler.cs
@@ -0,0 +1,76 @@
+﻿using BookIt.Application.Abstractions.Data;
+using BookIt.Application.Messaging;
+using BookIt.Domain;
+using BookIt.Domain.Abstractions;
+using Dapper;
+
+namespace BookIt.Application.Apartments.SearchApartments;
+
+internal sealed class SearchApartmentsQueryHandler 
+    : IqueryHandler<SearchApartmentsQuery, IReadOnlyList<ApartmentResponse>>
+{
+    private readonly ISqlConnectionFactory _sqlConnectionFactory;
+
+    private static readonly int[] ActiveBookingStatuses =
+    {
+        (int)BookingStatus.Reserved,
+        (int)BookingStatus.Confirmed,
+        (int)BookingStatus.Completed,
+    };
+
+    public SearchApartmentsQueryHandler(ISqlConnectionFactory sqlConnectionFactory)
+    {
+        _sqlConnectionFactory = sqlConnectionFactory;
+    }
+    
+    public async Task<Result<IReadOnlyList<ApartmentResponse>>> Handle(SearchApartmentsQuery request, CancellationToken token)
+    {
+        if (request.StartDate > request.EndDate)
+        {
+            return new List<ApartmentResponse>();
+        }
+
+        using var connection = _sqlConnectionFactory.CreateConnection();
+
+        const string sql = """
+                           SELECT
+                           a.id AS Id,
+                           a.name AS Name,
+                           a.description AS Description,
+                           a.price AS Price,
+                           a.price_currency AS Currency,
+                           a.address_country AS Country,
+                           a.address_state AS State,
+                           a.address_zip_code AS ZipCode,
+                           a.address_city AS City,
+                           a.address_street AS Street
+                           FROM apartments AS a
+                           WHERE NOT EXISTS(
+                               SELECT 1
+                               FROM bookings AS b
+                               WHERE
+                                   b.apartment_id = a.id AND 
+                                   b.start_date <= @EndDate AND
+                                   b.end_date >= @StartDate AND
+                                   b.status = ANY(@ActiveBookingStatuses)
+                           )
+                           """;
+
+        var apartments = await connection.QueryAsync<ApartmentResponse, AddressResponse, ApartmentResponse>(
+            sql,
+            (apartment, address) =>
+            {
+                apartment.Address = address;
+                return apartment;
+            },
+            new
+            {
+                request.StartDate,
+                request.EndDate,
+                ActiveBookingStatuses
+            },
+            splitOn: "Country"
+       );
+        return apartments.ToList();
+    }
+}
